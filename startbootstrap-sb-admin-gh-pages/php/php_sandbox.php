bec8db101b50dcf02dc24f2eaa802cc4aaa515dc
JuanDavidRT <jdramirezt@unal.edu.co>
2017-12-28 10:40:45 -0500

    Validación de datos y alerta de envío de datos

--- startbootstrap-sb-admin-gh-pages/php/php_sandbox.php
+++ startbootstrap-sb-admin-gh-pages/php/php_sandbox.php
@@ -1,57 +1,45 @@
 <?php
-
-
-echo'<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Prueba</title></head>
-<body>';
 // Datos del servidor
 $url = "72.29.85.225";
 $user = "edurappi";
 $password = "PS0m1fGAC1";
 $dbname = "edurappi_juandavid";
 
-$conn = new mysqli($servername, $username, $password, $dbname); // Conexión a la base de datos
+$conn = mysqli_connect($url,$user,$password,$dbname) or die('No se pudo realizar conexión'); // Conexión al servidor y mensaje de error
+
+//$dato1 = $dato2 = $dato3 = $dato4 = ""; // declaración de variables
+/*
+// A las variables se le asignan los datos del formularios/*
+$dato1 = $post['dato1'];
+$dato1 = $post['dato2'];
+$dato1 = $post['dato3'];
+$dato1 = $post['dato4'];
+*/
+/* // Test de datos
+
+echo $_POST['dato1'];
+echo $_POST['dato2'];
+echo $_POST['dato3'];
+echo $_POST['dato4'];
+*/
+
+// Query, las variables entran directamente por el POST
+$sql = "INSERT INTO php_sandbox (dato1, dato2, dato3 ,dato4) VALUES ('$_POST[dato1]', '$_POST[dato2]', '$_POST[dato3]', '$_POST[dato4]')"; // Query, las variables entran directamente
 
 
-if ($conn->connect_error) {
-    die("No es posible conectarse a la base de datos: " . $conn->connect_error); // Test de conexión
-}
-
-
-$dato1 = $dato2 = $dato3 = $dato4 = ""; // declaración de variables
-
-// A las variables se le asignan los datos del formularios
-$dato1 = $post('dato1');
-$dato1 = $post('dato2');
-$dato1 = $post('dato3');
-$dato1 = $post('dato4');
-
-$sql = "INSERT INTO php_sandbox (dato1, dato2, dato3 ,dato4) VALUES ($dato1, $dato2, $dato3, $dato4)"; // Query
-
+echo '
+ <script>
+ function alerta(){
+  alert("Datos enviados exitosamente");
+  }
+  </script>';
+//Confirmación de registro
 if ($conn->query($sql) === TRUE) {
-    echo "Registro agregado exitosamente";
+    echo '
+ <script>  alerta();  </script>';
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-
 $conn->close(); // Cerrar conexión
-
-// Conexión de mostrar datos
-/*
-$prueba = "SELECT correo_rappi,nombre1,nombre2 from soporte_activos_prueba";
-
-$resultado = $db->query($prueba);
-
-while($row = $resultado->fetch_assoc()){
-  echo "Name: " . $row["nombre1"]. " " . $row["nombre2"] . "<br>". "Correo: " . $row["correo_rappi"]. "<br>";
-}
-echo '
-*/
-</body>
-</html>';
  ?>
