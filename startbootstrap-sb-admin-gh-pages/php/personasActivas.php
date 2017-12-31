ffa03c55a2a9bac6a8a975984a3c5a17c227f178
JuanDavidRT <jdramirezt@unal.edu.co>
2017-12-31 16:14:14 -0500

    PHP para vista de personas acivas

---
+++ startbootstrap-sb-admin-gh-pages/php/personasActivas.php
@@ -0,0 +1,29 @@
+<?php
+
+
+echo'<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Prueba</title></head>
+<body>';
+
+$url = "72.29.85.225";
+$user = "edurappi";
+$password = "PS0m1fGAC1";
+$dbname = "edurappi_juandavid";
+
+$db = mysqli_connect($url,$user,$password,$dbname) or die('NOPE');
+
+$prueba = "SELECT nombre1,nombre2,correo_rappi from opeReallyNew";
+
+$resultado = $db->query($prueba);
+
+while($row = $resultado->fetch_assoc()){
+  echo "Name: " . $row["nombre1"]. " " . $row["nombre2"] . "<br>". "Correo: " . $row["correo_rappi"]. "<br>";
+}
+echo '
+</body>
+</html>';
+ ?>
