deb57b0f9891e5033bce44b199e176824f93a404
JuanDavidRT <jdramirezt@unal.edu.co>
2018-01-02 21:37:27 -0500

    Actualización de todos los archivos con los del servidor

---
+++ startbootstrap-sb-admin-gh-pages/php/modificarDatos.php
@@ -0,0 +1,30 @@
+<?php
+// Datos del servidor
+$url = "72.29.85.225";
+$user = "edurappi";
+$password = "PS0m1fGAC1";
+$dbname = "edurappi_juandavid";
+
+$conn = mysqli_connect($url,$user,$password,$dbname) or die('No se pudo realizar conexión'); // Conexión al servidor y mensaje de error
+
+// Query, las variables entran directamente por el POST
+$sql = "UPDATE opeReallyNew SET $_POST[campo] = '$_POST[nuevo]' WHERE email_rappi = '$_POST[email_rappi]'"; // Query, las variables entran directamente
+$sql2 = "INSERT modif_opeReallyNew (email_rappi, dato_modificado) VALUES ('$_POST[email_rappi]','$_POST[campo]')";
+
+
+echo '
+ <script>
+ function alerta(){
+  alert("Datos modificados exitosamente");
+  }
+  </script>';
+//Confirmación de registro
+if ($conn->query($sql) === TRUE & $conn->query($sql2) === TRUE) {
+    echo '
+ <script>  alerta();  </script>';
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close(); // Cerrar conexión
+ ?>
--- startbootstrap-sb-admin-gh-pages/php/personasActivas.php
+++ startbootstrap-sb-admin-gh-pages/php/personasActivas.php
@@ -5,8 +5,8 @@ echo'<!DOCTYPE html PUBLIC"-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Prueba</title></head>
+<meta http-equiv="content-type" content="text/html; charset=utf8_spanish_ci" />
+<title>Personas Activas</title></head>
 <body>';
 
 $url = "72.29.85.225";
@@ -16,14 +16,44 @@ $dbname = "edurappi_juandavid";
 
 $db = mysqli_connect($url,$user,$password,$dbname) or die('NOPE');
 
-$prueba = "SELECT nombre1,nombre2,correo_rappi from opeReallyNew";
+$prueba = "SELECT email_rappi,nombre1,nombre2, apellido1, apellido2, celular, fecha_ingreso, rol, modalidad, lider, turno, dia_d, pais from opeReallyNew";
 
 $resultado = $db->query($prueba);
+echo "<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0' >
+  <tr>
+    <th>Nombres</th>
+    <th>Apellidos</th>
+    <th>Correo Rappi</th>
+    <th>Celular</th>
+    <th>Fecha Ingreso</th>
+    <th>Rol</th>
+    <th>Modalidad</th>
+    <th>Lider</th>
+    <th>Turno</th>
+    <th>Dia de Descanso</th>
+    <th>Pais</th>
+  </tr>";
 
 while($row = $resultado->fetch_assoc()){
-  echo "Name: " . $row["nombre1"]. " " . $row["nombre2"] . "<br>". "Correo: " . $row["correo_rappi"]. "<br>";
+
+  echo "<tr>
+    <td>" . $row["nombre1"]. " " . $row["nombre2"] . "</td>
+    <td>" . $row["apellido1"]. " " . $row["apellido2"] . "</td>
+    <td> " . $row["email_rappi"]. "</td>
+    <td> " . $row["celular"]. "</td>
+    <td> " . $row["fecha_ingreso"]. "</td>
+    <td> " . $row["rol"]. "</td>
+    <td> " . $row["modalidad"]. "</td>
+    <td> " . $row["lider"]. "</td>
+    <td> " . $row["turno"]. "</td>
+    <td> " . $row["dia_d"]. "</td>
+    <td> " . $row["pais"]. "</td>
+  </tr> "
+;
+
 }
 echo '
+</table>
 </body>
 </html>';
  ?>
